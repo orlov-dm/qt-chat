8ec887d4f8a22975ff241dd7cd01e9648a82ff4b
Dmitriy Orlov <dm.ed.orlov@gmail.com>
2017-07-21 21:07:14 +0300

    feat: Server ui - start/stop

--- ChatServer/ui/mainwindow.cpp
+++ ChatServer/ui/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "core/server.h"
+
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,13 +12,59 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
     QString ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
-    QRegExp ipRegex (QString("^%1\\.%2\\.%3\\.%4$").arg(ipRange, ipRange, ipRange, ipRange));
+    QRegExp ipRegex (QString("^(%1\\.%2\\.%3\\.%4)|localhost$").arg(ipRange, ipRange, ipRange, ipRange));
 
     auto ipValidator = new QRegExpValidator(ipRegex, ui->leIPAddress);
-    ui->leIPAddress->setValidator( ipValidator );
+    ui->leIPAddress->setValidator( ipValidator );    
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pbStartServer_clicked()
+{
+    auto validator = ui->leIPAddress->validator();
+    auto ipString = ui->leIPAddress->text();
+    if(ipString.isEmpty())
+    {
+        QMessageBox::critical(this, tr("Error"), tr("IP is empty"));
+        return;
+    }
+    int invalidPosition;
+    if(!validator
+            || validator->validate(ipString, invalidPosition) != QValidator::Acceptable)
+    {
+        QMessageBox::critical(this, tr("Error"), tr("IP is invalid"));
+        return;
+    }
+    auto port = ui->sbPort->value();
+    if(!port) {
+        port = Server::DEFAULT_PORT;
+        QMessageBox::warning(this, tr("Warning"), tr("Port is 0. Will be using default port"));
+        ui->sbPort->setValue(port);
+    }
+    QHostAddress hostAddress(ipString);
+    if(Server::getInstance().listen(hostAddress, port))
+    {
+        QMessageBox::information(this, tr("Information"), tr("Server successfully started: %1:%2").arg(ipString).arg(port));
+        ui->pbStartServer->setEnabled(false);
+        ui->pbStopServer->setEnabled(true);
+    }
+    else
+    {
+        QMessageBox::critical(this, tr("Error"), tr("Server cannot be started: %1:%2").arg(ipString).arg(port));
+    }
+}
+
+void MainWindow::on_pbStopServer_clicked()
+{
+    if(Server::getInstance().isListening())
+    {
+        Server::getInstance().close();
+        QMessageBox::information(this, tr("Information"), tr("Server successfully stopped"));
+        ui->pbStartServer->setEnabled(true);
+        ui->pbStopServer->setEnabled(false);
+    }
 }
